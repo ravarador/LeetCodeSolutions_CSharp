c60e6d4e87285eda83efa28a8c6aef0939e74e38
Ravni Arador <ravarador@gmail.com>
2021-08-23 16:45:10 -0400

    Solved PalindromeNumber

---
+++ Easy Problems/PalindromeNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions_CSharp.Easy_Problems
+{
+    public static class PalindromeNumber
+    {
+        //Runtime: 76 ms, faster than 30.08% of C# online submissions for Palindrome Number.
+        //Memory Usage: 17.7 MB, less than 31.47% of C# online submissions for Palindrome Number.
+        public static bool IsPalindrome(int x)
+        {
+            var arr = x.ToString().ToCharArray();
+
+            for(int i = 0; i < arr.Length - 1; i++)
+            {
+                if(arr[i] != arr[arr.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace LeetCodeSolutions_CSharp
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
